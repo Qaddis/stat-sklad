72570f3ebca074d43343d365a42b9248425ea412
Qaddis <svyatoslavb1107@gmail.com>
2025-11-22 10:56:32 +0300

    Backend: add db connection

---
+++ backend/src/db/database.py
@@ -0,0 +1,15 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+from sqlalchemy.orm import DeclarativeBase
+
+from config import settings
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+engine = create_async_engine(
+    url=settings.DB_URL, echo=(False if settings.MODE == "PROD" else True)
+)
+
+session_factory = async_sessionmaker(engine, expire_on_commit=False)
