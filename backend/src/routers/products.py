26072fab1ab77544293b93cbca34682eb65df8ce
19Akvarelka91 <sergey0907bil.5565@gmail.com>
2025-12-03 00:41:14 +0300

    Добавление Постраничной пагинации,дай бог работает

--- backend/src/routers/products.py
+++ backend/src/routers/products.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, HTTPException, status
+from fastapi import APIRouter, HTTPException, status, Query
 
 from src.services.products import get_all_products, get_product_by_id
 
@@ -7,9 +7,11 @@ router = APIRouter(prefix="/products", tags=["Products"])
 
 
 @router.get("/")
-async def get_products():
-    return get_all_products()
-
+async def get_products(
+    page: int = Query(1, ge=1, description="Номер страницы"),
+    products_per_page: int = Query(25, ge=1, le=100, description="Записей на странице")
+):
+    return get_all_products(page=page, products_per_page=products_per_page) # Вывод странички по номеру в адресе
 
 @router.get("/{product_id}")
 async def get_product(product_id: int):
@@ -20,5 +22,4 @@ async def get_product(product_id: int):
             status_code=status.HTTP_404_NOT_FOUND,
             detail=f"Продукт с id = {product_id} не найден",
         )
-
     return product
--- backend/src/services/products.py
+++ backend/src/services/products.py
@@ -5,9 +5,29 @@ products = {
 }
 
 
-def get_all_products():
-    return [product for product in products.values()]
+def get_all_products(page: int = 1, products_per_page: int = 25):
+    all_products = list(products.values())
+    total = len(all_products)  # Всего элементов
 
+    skip = (page - 1) * products_per_page # Элементы по номеру страницы
+    if skip >= total: # "Ошибка пользователя при запросе несуществующей страницы"
+        return {
+            "products": [], # "Список товаров закончился"
+            "total": total,
+            "page": page,
+            "per_page": products_per_page,
+            "total_pages": (total + products_per_page - 1) // products_per_page,
+            "has_next": False   # Далее не идем по страницам
+        }
+    paginated = all_products[skip:skip + products_per_page] # Ввыводим кол - во элементов постранично(пагинация же да)
+    return {    # Вывод продуктов постранично, и данных о текущей странице, а так же расчет следующей
+         "products": paginated,
+        "total": total,
+        "page": page,
+        "per_page": products_per_page,
+        "total_pages": (total + products_per_page - 1) // products_per_page,
+        "has_next": (skip + products_per_page) < total
+    }
 
 def get_product_by_id(id: int):
     return products.get(id)
