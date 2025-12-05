154ef76130ca9ddc2d5f00b9b7e0c01ce003097d
bebdeb <romab9386@gmail.com>
2025-12-06 00:26:07 +0300

    Backend: generate new .pem jwt keys and switch path to them

--- backend/src/config.py
+++ backend/src/config.py
@@ -7,8 +7,8 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 BASE_DIR = Path(__file__).parent.parent
 
 class AuthJWT(BaseModel):
-    private_key_path: Path = BASE_DIR / "src" / "certs" / "private.key"
-    public_key_path: Path = BASE_DIR / "src" / "certs" / "public.key"
+    private_key_path: Path = BASE_DIR / "src" / "certs" / "jwt-private.pem"
+    public_key_path: Path = BASE_DIR / "src" / "certs" / "jwt-public.pem"
     algorihm: str = "RS256"
     access_token_expire_minutes: int = 15
     refresh_token_expire_days: int = 30
