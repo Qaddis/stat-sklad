0355ee46c44b12dc9176af3b56a52ba4b343d1d7
bebdeb <romab9386@gmail.com>
2025-12-06 13:49:08 +0300

    Backend: change access token expire time to 60 minutes

--- backend/src/config.py
+++ backend/src/config.py
@@ -10,7 +10,7 @@ class AuthJWT(BaseModel):
     private_key_path: Path = BASE_DIR / "src" / "certs" / "jwt-private.pem"
     public_key_path: Path = BASE_DIR / "src" / "certs" / "jwt-public.pem"
     algorihm: str = "RS256"
-    access_token_expire_minutes: int = 15
+    access_token_expire_minutes: int = 60
     refresh_token_expire_days: int = 30
 
 
