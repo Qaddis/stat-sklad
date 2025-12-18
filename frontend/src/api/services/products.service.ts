cd09f8e2b13fa03c4bf15326bd89d91df9dcae0d
Qaddis <svyatoslavb1107@gmail.com>
2025-12-18 18:57:44 +0300

    Frontend: add receiving and displaying a list of products to the Dashboard page

--- frontend/src/api/services/products.service.ts
+++ frontend/src/api/services/products.service.ts
@@ -1,11 +1,21 @@
 import type { ActionTypeEnum, IActionFormData } from "@/types/actions.types"
 import type { IResponseResult } from "@/types/api.types"
+import type { IPaginateProducts } from "@/types/products.types"
 
 import { EndpointsEnum } from "@/constants/api.constants"
 import { apiErrorCatch } from "@/utils/api.utils"
 import instance from "../api.interceptor"
 
 class ProductsService {
+	async get(size: number, page: number, q: string): Promise<IPaginateProducts> {
+		const response = await instance.get<IPaginateProducts>(
+			EndpointsEnum.PRODUCTS,
+			{ params: { size, page, q } }
+		)
+
+		return response.data
+	}
+
 	async action(
 		actionType: keyof typeof ActionTypeEnum,
 		data: IActionFormData
