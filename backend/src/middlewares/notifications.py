86ad9584999030d2453c6e4084cdcf071ea3c723
Qaddis <svyatoslavb1107@gmail.com>
2025-12-18 14:21:53 +0300

    Backend: fix title generation for Notification

--- backend/src/middlewares/notifications.py
+++ backend/src/middlewares/notifications.py
@@ -5,12 +5,13 @@ from ..models import UnitsEnum, NotificationModel
 from ..db import session_factory
 from ..repositories import ActionsCRUD, NotificationCRUD
 
-KILOGRAM_LIMIT = 30
-PIECES_LIMIT = 12
+KILOGRAM_LIMIT = 5
+PIECES_LIMIT = 10
+
 
 async def off_product(supply: CreateSupply):
     async with session_factory() as db:
-    
+
         not_crud = NotificationCRUD(db)
         supply = supply.model_dump()
         supply = supply["supply_content"]
@@ -20,35 +21,52 @@ async def off_product(supply: CreateSupply):
             notification_obj = None
             if existed_product:
                 if existed_product.ingredient.units == UnitsEnum.KILOGRAMS:
-                    if (existed_product.quantity - item["quantity"] < KILOGRAM_LIMIT) and await not_crud.get_notification(existed_product.ingredient_id):
-                        await not_crud.edit_notification(f"Внимание! На складе осталось всего {existed_product.quantity + item["quantity"]} шт. товара {existed_product.ingredient.name}",
-                        existed_product.ingredient_id)
+                    if (
+                        existed_product.quantity - item["quantity"] < KILOGRAM_LIMIT
+                    ) and await not_crud.get_notification(
+                        existed_product.ingredient_id
+                    ):
+                        await not_crud.edit_notification(
+                            f"Внимание! На складе осталось всего {existed_product.quantity + item["quantity"]} шт. товара {existed_product.ingredient.name}",
+                            existed_product.ingredient_id,
+                        )
                         await db.commit()
                     elif existed_product.quantity - item["quantity"] < KILOGRAM_LIMIT:
-                        notification_obj = Notification(title=existed_product.ingredient.id,
-                        description=f"Внимание! На складе осталось всего {existed_product.quantity - item["quantity"]} кг. товара {existed_product.ingredient.name}")
+                        notification_obj = Notification(
+                            title=f"Истощение запасов: {existed_product.ingredient.name}",
+                            description=f"Внимание! На складе осталось всего {existed_product.quantity - item["quantity"]} кг. товара {existed_product.ingredient.name}",
+                        )
 
                 else:
-                    if (existed_product.quantity - item["quantity"] < PIECES_LIMIT) and await not_crud.get_notification(existed_product.ingredient_id):
-                        await not_crud.edit_notification(f"Внимание! На складе осталось всего {existed_product.quantity - item["quantity"]} шт. товара {existed_product.ingredient.name}",
-                        existed_product.ingredient_id)
+                    if (
+                        existed_product.quantity - item["quantity"] < PIECES_LIMIT
+                    ) and await not_crud.get_notification(
+                        existed_product.ingredient_id
+                    ):
+                        await not_crud.edit_notification(
+                            f"Внимание! На складе осталось всего {existed_product.quantity - item["quantity"]} шт. товара {existed_product.ingredient.name}",
+                            existed_product.ingredient_id,
+                        )
                         await db.commit()
                     elif existed_product.quantity - item["quantity"] < PIECES_LIMIT:
-                        notification_obj = Notification(title=existed_product.ingredient.name,
-                        description=f"Внимание! На складе осталось всего {existed_product.quantity - item["quantity"]} шт. товара {existed_product.ingredient.name}")
+                        notification_obj = Notification(
+                            title=f"Истощение запасов: {existed_product.ingredient.name}",
+                            description=f"Внимание! На складе осталось всего {existed_product.quantity - item["quantity"]} шт. товара {existed_product.ingredient.name}",
+                        )
                 if notification_obj:
                     all_notifications.append(notification_obj)
             else:
                 pass
-            
+
         if all_notifications != []:
             for notification in all_notifications:
                 await not_crud.save_notification(notification, item["ingredient_id"])
             await db.commit()
-            
+
+
 async def add_product(supply: CreateSupply):
     async with session_factory() as db:
-    
+
         not_crud = NotificationCRUD(db)
         supply = supply.model_dump()
         supply = supply["supply_content"]
@@ -57,22 +75,34 @@ async def add_product(supply: CreateSupply):
             existed_product = await not_crud.get_product(item["ingredient_id"])
             if existed_product:
                 if existed_product.ingredient.units == UnitsEnum.KILOGRAMS:
-                    if (existed_product.quantity + item["quantity"] < KILOGRAM_LIMIT) and await not_crud.get_notification(existed_product.ingredient_id):
-                        await not_crud.edit_notification(f"Внимание! На складе осталось всего {existed_product.quantity + item["quantity"]} шт. товара {existed_product.ingredient.name}",
-                        existed_product.ingredient_id)
+                    if (
+                        existed_product.quantity + item["quantity"] < KILOGRAM_LIMIT
+                    ) and await not_crud.get_notification(
+                        existed_product.ingredient_id
+                    ):
+                        await not_crud.edit_notification(
+                            f"Внимание! На складе осталось всего {existed_product.quantity + item["quantity"]} шт. товара {existed_product.ingredient.name}",
+                            existed_product.ingredient_id,
+                        )
                         await db.commit()
                     elif existed_product.quantity + item["quantity"] > KILOGRAM_LIMIT:
                         all_notifications.append(existed_product.ingredient_id)
                 else:
-                    if (existed_product.quantity + item["quantity"] < PIECES_LIMIT) and await not_crud.get_notification(existed_product.ingredient_id):
-                        await not_crud.edit_notification(f"Внимание! На складе осталось всего {existed_product.quantity + item["quantity"]} шт. товара {existed_product.ingredient.name}",
-                        existed_product.ingredient_id)
+                    if (
+                        existed_product.quantity + item["quantity"] < PIECES_LIMIT
+                    ) and await not_crud.get_notification(
+                        existed_product.ingredient_id
+                    ):
+                        await not_crud.edit_notification(
+                            f"Внимание! На складе осталось всего {existed_product.quantity + item["quantity"]} шт. товара {existed_product.ingredient.name}",
+                            existed_product.ingredient_id,
+                        )
                         await db.commit()
                     elif existed_product.quantity + item["quantity"] > PIECES_LIMIT:
                         all_notifications.append(existed_product.ingredient_id)
             else:
                 pass
-                        
+
         if all_notifications != []:
             for notification in all_notifications:
                 await not_crud.del_notification(notification)
--- backend/src/schemas/notification.py
+++ backend/src/schemas/notification.py
@@ -3,18 +3,22 @@ from typing import List
 from datetime import datetime
 from uuid import UUID
 
+
 class Notification(BaseModel):
-  	title: str
-  	description: str
-   
+    title: str
+    description: str
+
+
 class Notification_list(BaseModel):
     content: List[Notification]
-    
+
+
 class NotificationAnswer(BaseModel):
-  	id: UUID
-  	title: str
-  	description: str
-  	created_at: datetime
-   
+    id: UUID
+    title: str
+    description: str
+    created_at: datetime
+
+
 class NotificationAnswer_list(BaseModel):
     content: List[NotificationAnswer]
